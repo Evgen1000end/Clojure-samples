19d72978af1465106fcea6cc79899f1dd06c97a3
evgen1000end <evgen1000end@gmail.com>
2015-08-03 19:20:14 +0300

    Quick Start

--- clojure-samples/src/clojure_samples/L0.clj
+++ clojure-samples/src/clojure_samples/L0.clj
@@ -9,3 +9,30 @@
 
 ; Print with default fraction format
 (println  (average [1 3 4 5]))
+
+; simple expression
+(+ 1 2)
+
+
+(def a 1)
+(inc a)
+
+; а - не увеличился
+(println a)
+
+; а так - правильно!
+(println (inc a))
+
+; call Java
+(println (Math/pow 2 10))
+
+
+;Deserialize
+(println (read-string "42"))
+
+;Serialize;
+(def a (pr-str 2))
+(println a)
+
+
+
